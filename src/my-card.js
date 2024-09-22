a137777eeb62adce80f200e6af47cdc461545d50
Eglicky <163663786+Eglicky@users.noreply.github.com>
2024-09-22 11:27:20 -0400

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,8 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "Epic Racoon";
+    this.imageUrl = "https://media.istockphoto.com/id/1154370446/photo/funny-raccoon-in-green-sunglasses-showing-a-rock-gesture-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=kkZiaB9Q-GbY5gjf6WWURzEpLzNrpjZp_tn09GB21bI=";
   }
 
   static get styles() {
@@ -23,17 +24,86 @@ export class MyCard extends LitElement {
         display: block;
       }
     `;
+  
+
+  img {
+    width: 200px;
+    padding: 10px 0;
   }
 
+  .epic {
+    font-size: 10px;
+    padding: 10px 0;
+  }
+
+  .epiccard { 
+    background-color: blue;
+    width: 300px;
+    text-align: clear;
+    border-radius: 32px;
+    margin: auto;
+  }
+
+  #background-change {
+    background-color: orange;
+  }
+
+  .epiccard-wrap {
+    margin-bottom: 16px;
+  }
+
+  button {
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #f0f0f0;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .epicbuttonwow:hover {
+    background-color: blue;
+  }
+
+
+  
   render() {
     return html`<div>${this.title}</div>`;
-  }
+    
+    <h2>Controls</h2>
+    <div class="epiccard-wrap">
+      <button class="duplicate">Clone Card</button>
+      <button id="bg-change">Change background</button>
+    </div>
+
+    <div id="cardid">
+      <div class="epiccard">
+        <img src="${this.imageUrl}" alt="Raccoon Image">
+        <div>
+          <a href="https://www.youtube.com/watch?v=F2YpXC1itEE" class="epic">
+            <button class="epicbuttonwow">Epic Raccoon Video</button>
+          </a>
+        </div>
+        <button id="yay">Make Another</button>
+        <button id="change-title">Change Title</button>
+        <button id="change-image">Change Image</button>
+      </div>
+    </div>
+  `;
+}
+
+}
 
   static get properties() {
     return {
       title: { type: String },
+      imageUrl: {type: String}
     };
   }
 }
 
+  
+
 globalThis.customElements.define(MyCard.tag, MyCard);
+
+
