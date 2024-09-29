1dbb8b7f50ef7454894c6e83de6aec0305f03966
Eglicky <163663786+Eglicky@users.noreply.github.com>
2024-09-29 08:45:31 -0400

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -15,6 +15,7 @@ export class MyCard extends LitElement {
     super();
     this.title = "Epic Raccoon";
     this.imageUrl = "https://media.istockphoto.com/id/1154370446/photo/funny-raccoon-in-green-sunglasses-showing-a-rock-gesture-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=kkZiaB9Q-GbY5gjf6WWURzEpLzNrpjZp_tn09GB21bI=";
+    this.fancy = false;
   }
 
   static get styles() {
@@ -22,6 +23,16 @@ export class MyCard extends LitElement {
       :host {
         display: block;
         font-family: Arial;
+      }
+
+      :host(.fancy){
+        .epiccard{
+        background-color: pink;
+        width: 300px;
+        text-align: center;
+        border-radius: 32px;
+        margin: auto;
+        }
       }
 
       img {
@@ -63,17 +74,19 @@ export class MyCard extends LitElement {
         background-color: blue;
       }
     `;
-  }
+    
+}
+
 
   render() {
     return html`
       <div class="epiccard">
         <h1>${this.title}</h1>
         <p>${this.text}</p>
-        <details ?open="${this.fancy}">
+        <details @toggle="${this.fancyToggle}">
           <summary>Description</summary>
           <div>
-            <slot>This is an Epic Raccon that loves to dance.</slot>
+            <slot>This is an EPIC Raccon that loves to ROCK. ROCK AND ROLL!</slot>
           </div>
         </details>
         <img src="${this.imageUrl}" alt="Raccoon Image">
@@ -96,10 +109,20 @@ export class MyCard extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      imageUrl: { type: String }
+      imageUrl: { type: String },
+      fancy: {type: Boolean}
     };
   }
-}
 
+
+fancyToggle(event) {
+  this.fancy = event.target.open;
+  if (this.fancy) {
+    this.classList.add('fancy');
+  } else {
+    this.classList.remove('fancy');
+    }
+  }
+}
 // Register the custom element with the browser
 customElements.define(MyCard.tag, MyCard);
